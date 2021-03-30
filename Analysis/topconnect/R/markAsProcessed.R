04d118f56d9f6c83c70b04efa658e68db264bb97
Mark Bower <mark.bower@yale.edu>
2021-03-30 13:08:56 -0600

    modified markAsProcessed

--- Analysis/topconnect/R/markAsProcessed.R
+++ Analysis/topconnect/R/markAsProcessed.R
@@ -11,7 +11,7 @@ markAsProcessed <- function( dbName, table, subject, channel, suid, timestamp, f
   #' markAsProcessed( conn, case, flag=1 )
 
   print( "In markAsProcessed" )
-  conn <- topconnect::db( dbName, host=hostname, password=password )
+  conn <- topconnect::db( db_user="root", dbname=dbName, host=hostname, password=password )
   query <- paste0( 'update ', table, ' set done=',flag,' where subject=\'',subject,'\' AND session=\'',suid,'\' AND channel=\'',channel,'\' AND timestamp=',timestamp,';' )
   DBI::dbGetQuery( conn, query )
   DBI::dbDisconnect( conn )
