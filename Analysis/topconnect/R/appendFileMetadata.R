d637c6e99fd6b0f2984f0ab7f4fdea3f07642d12
Mark Bower <mark.bower@yale.edu>
2022-04-15 19:12:10 -0600

    Added some print statements #5

--- Analysis/topconnect/R/appendFileMetadata.R
+++ Analysis/topconnect/R/appendFileMetadata.R
@@ -5,6 +5,7 @@ appendFileMetadata <- function( compArgs, filename ) {
   conn <- db_provider$connect()
   print( paste0( "topconnect :: conn: ", !is.null(conn) ) )
   mi <- RFactories::metadataInformer( filename=filename, compArgs=compArgs)
+  print( paste0( "RFactories :: mi: ", !is.null(mi) ) )
   compArgs$add( mi )
   DBI::dbDisconnect( conn )
   return( compArgs )
